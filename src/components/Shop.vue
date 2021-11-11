35429dd277e2cf1c562a1fdcb507a006cc699e0d
WICHUDA CHANTHONG <6331305026@lamduan.mfu.ac.th>
2021-11-11 16:27:05 +0700

    update

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -5,7 +5,7 @@
         <v-col cols="10">
           <v-row align="center" justify="center">
             
-            <v-card
+            <v-sheet
               class="mx-auto my-12"
               max-width="350"
               dark="true"
@@ -61,24 +61,8 @@
               </div>
               </v-row>
               </v-card-actions>
-            </v-card>
-            <!-- <v-col sm="4" class="hidden-xs-only">
-              <v-img src="@/assets/img/ill2.svg" class="d-block ml-auto mr-auto" max-width="350px" />
-            </v-col>
-            <v-col cols="12" sm="8" class="white--text text-left">
-              <h1 class="font-weight-light display-2 mb-2">Baixar Demonstração</h1>
-              <h1 class="font-weight-light">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ex
-                animi quod laboriosam vel blanditiis labore alias, aliquid,
-                tempora repellendus non.
-              </h1>
-              <v-btn rounded outlined href="https://github.com/Joabsonlg/Landing-Page" target="_blank" large color="white" class="mt-4">
-                <v-icon class="mr-2">
-                  mdi-github
-                </v-icon>
-                Git Hub
-              </v-btn>
-            </v-col> -->
+            </v-sheet>
+
           </v-row>
         </v-col>
       </v-row>
@@ -96,7 +80,7 @@
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  height: 680px;
+  height: 690px;
 }
 
 #shop .container,
